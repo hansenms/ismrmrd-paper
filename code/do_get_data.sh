5f10066fad40014dabe7f01584acb0daa387a5ab
Souheil Inati <souheil.inati@gmail.com>
2015-10-27 23:08:59 -0400

    Minor edits.

--- code/do_get_data.sh
+++ code/do_get_data.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# Generate synthetic data using C++ program
-ismrmrd_generate_cartesian_shepp_logan -m 512 -o synth.h5
-
 # Copy the scanner data here
 curl -L -o ismrmrd_data.zip https://zenodo.org/record/32675/files/ismrmrd_data.zip
 unzip ismrmrd_data.zip
