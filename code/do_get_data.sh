46425fab08f0771d2fba60530928e00ec647613c
Souheil Inati <souheil.inati@gmail.com>
2015-10-29 11:33:23 -0400

    Moved to new url.

--- code/do_get_data.sh
+++ code/do_get_data.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Copy the scanner data here
-curl -L -o ismrmrd_data.zip https://zenodo.org/record/32675/files/ismrmrd_data.zip
+curl -L -o ismrmrd_data.zip https://zenodo.org/record/32809/files/ismrmrd_data.zip
 unzip ismrmrd_data.zip
 find ismrmrd_data -name "*.h5" -exec cp {} . \; -print
 
